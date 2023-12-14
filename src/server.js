4310ec2823c4f88f33c0272508515a0d42984b34
Mikael Marceniuk <mika.marceniuk@gmail.com>
2023-12-14 07:23:38 -0300

    feat: Implemented query and params readers

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 import http from "node:http"
 import router from "./router.js"
+import extractQueryParams from "./utils/extractQueryParams.js"
 
 const server = http.createServer((req, res) => {
   const { method, url } = req
   console.log(method, url)
 
   const route = router.find((r) => r.method == method && r.url.test(url))
-  if (route) return route.handler(req, res)
+  if (route) {
+    const { query, ...params } = url.match(route.url).groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
 
   res.writeHead(404).end()
 })
